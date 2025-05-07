ff69549566cbc79f9548d24e6fbd371087c2b304
Chumar <chumarsarwar3@gmail.com>
2025-05-07 15:32:55 +0500

    db connection for production

--- src/libs/mongodb.js
+++ src/libs/mongodb.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
-// const database =
-  // 'mongodb+srv://2223016:N6bVPiFwmE5hhoI5@cluster0.3dvrp.mongodb.net/Inventoryservices?retryWrites=true&w=majority'
-const database = "mongodb://localhost:27017/Inventoryservices";
+const database =
+  'mongodb+srv://2223016:N6bVPiFwmE5hhoI5@cluster0.3dvrp.mongodb.net/Inventoryservices?retryWrites=true&w=majority'
+// const database = "mongodb://localhost:27017/Inventoryservices";
 let isConnected = false
 
 export const connectToDB = async () => {
